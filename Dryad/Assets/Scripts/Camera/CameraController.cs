d48568900145552995520309f64e83335e1f6538
Brunosaur <mr.bruno.bouchard@gmail.com>
2015-11-22 21:15:56 -0500

    Added player prefab

---
+++ Dryad/Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+    public GameObject referenceTracker;
+    public float cameraXDamping = 1.0f;
+    private Vector3 myTransform;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        myTransform = this.transform.position;
+        myTransform.x = DampingUtility.Damp(myTransform.x, referenceTracker.transform.position.x, cameraXDamping, TimeHelper.GameTime);
+        this.transform.position = myTransform;
+	}
+}
