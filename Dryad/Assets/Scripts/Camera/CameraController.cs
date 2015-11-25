8377e2db075d43ed295408493bda3fd99cf4003f
DarkGizmo <francois.vaillancourt@hotmail.ca>
2015-11-24 23:21:13 -0500

    First pass on throw and 2D line drawing

--- Dryad/Assets/Scripts/Camera/CameraController.cs
+++ Dryad/Assets/Scripts/Camera/CameraController.cs
@@ -1,21 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CameraController : MonoBehaviour {
+public class CameraController : MonoBehaviour
+{
     public GameObject referenceTracker;
     public float cameraXDamping = 1.0f;
-    private Vector3 myTransform;
-
-	// Use this for initialization
-	void Start () {
-	
-	}
+  
 	
 	// Update is called once per frame
 	void Update ()
     {
-        myTransform = this.transform.position;
-        myTransform.x = DampingUtility.Damp(myTransform.x, referenceTracker.transform.position.x, cameraXDamping, TimeHelper.GameTime);
-        this.transform.position = myTransform;
+        Vector3 camPosition = this.transform.position;
+        camPosition.x = DampingUtility.Damp(camPosition.x, referenceTracker.transform.position.x, cameraXDamping, TimeHelper.GameTime);
+        this.transform.position = camPosition;
 	}
 }
--- Dryad/Assets/Scripts/Gameplay/Player/PlayerController.cs
+++ Dryad/Assets/Scripts/Gameplay/Player/PlayerController.cs
@@ -1,7 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController
+    : MonoBehaviour
+    , PostRendererListenerInterface
+{
     public float force = 30.0f;
     public float flapIntensity = 5.0f;
     public bool stateFree = true;
@@ -9,12 +12,19 @@ public class PlayerController : MonoBehaviour {
     private bool isAnchored = false;
 
     public GameObject BallisticAmmo;
-    private float BallisticLaunchForce;
-    public float LaunchForcePerSecond = 5.0f;
+    public float BallisticLaunchForce = 1.0f;
+    public float BallisticFactor = 0.3f;
+
+    public Material ArrowMaterial;
+
+    private bool isAiming;
+    private Vector3 originalAimingPosition;
+    private Vector3 currentAimingPosition;
 
     // Use this for initialization
-    void Start () {
-	
+    void Start ()
+    {
+        Camera.main.GetComponent<PostRendererEmitter>().RegisterPostRendererListener(this);
 	}
 
     // Update is called once per frame
@@ -42,24 +52,45 @@ public class PlayerController : MonoBehaviour {
                 isAnchored = true;
                 this.GetComponent<SpriteRenderer>().color = Color.red;
             }
+        }
+        GetComponent<Rigidbody2D>().velocity = myVelocity;
 
-            if (isAnchored)
+        if (isAnchored)
+        {
+            if (!isAiming && Input.GetButtonDown("Fire"))
             {
-                if(Input.GetButton("Fire"))
+                originalAimingPosition = GetMouseScreenPosition();
+                currentAimingPosition = GetMouseScreenPosition();
+                isAiming = true;
+            }
+            else if (isAiming)
+            {
+                if (Input.GetButtonUp("Fire"))
                 {
-                    BallisticLaunchForce += TimeHelper.GameTime * LaunchForcePerSecond;
-                    
-                }
-                else if (BallisticLaunchForce > 0.0f)
-                {
-                    GameObject ballistic = (GameObject)GameObject.Instantiate(BallisticAmmo, transform.position, Quaternion.identity);
-                    ballistic.GetComponent<Rigidbody2D>().AddForce((GetMouseWorldPosition() - transform.position).normalized * BallisticLaunchForce);
+                    isAiming = false;
 
-                    BallisticLaunchForce = 0.0f;
+                    GameObject ballistic = (GameObject)GameObject.Instantiate(BallisticAmmo, transform.position, Quaternion.identity);
+
+                    Vector3 rangeVector = ScreenToWorldPoint(originalAimingPosition) - ScreenToWorldPoint(currentAimingPosition);
+
+                    ballistic.GetComponent<Rigidbody2D>().AddForce(GetShootingRatio() * BallisticLaunchForce  * rangeVector.normalized);
+                }
+                else
+                {
+                    currentAimingPosition = GetMouseScreenPosition();
                 }
             }
         }
-        GetComponent<Rigidbody2D>().velocity = myVelocity;
+    }
+
+    private float GetShootingRatio()
+    {
+        //1 - e ^ (-x / .3)
+        Vector3 rangeVector = ScreenToWorldPoint(originalAimingPosition) - ScreenToWorldPoint(currentAimingPosition);
+
+        float factor = 1.0f - Mathf.Exp(-rangeVector.magnitude / BallisticFactor);
+
+        return factor;
     }
 
     private Vector3 GetMouseWorldPosition()
@@ -71,5 +102,59 @@ public class PlayerController : MonoBehaviour {
         Vector3 position = ray.GetPoint(distance);
 
         return position;
+    }
+
+    public float x, y;
+
+    public Vector3 WorldToScreenPoint(Vector3 world)
+    {
+        Vector3 screenPoint = world - Camera.main.transform.position;
+        screenPoint.Set(screenPoint.x / Camera.main.orthographicSize * 0.5f / Camera.main.aspect + 0.5f, screenPoint.y / Camera.main.orthographicSize * 0.5f + 0.5f, 0.0f);
+        return screenPoint;
+    }
+
+    public Vector3 ScreenToWorldPoint(Vector3 screenPoint)
+    {
+        Vector3 worldPoint = new Vector3((screenPoint.x - 0.5f) * (Camera.main.orthographicSize * 2.0f * Camera.main.aspect), (screenPoint.y - 0.5f) * (Camera.main.orthographicSize * 2.0f));
+        worldPoint.Set(worldPoint.x + Camera.main.transform.position.x, worldPoint.y + Camera.main.transform.position.y, 0.0f);
+
+        return worldPoint;
+    }
+
+    public Vector3 GetMouseScreenPosition()
+    {
+        Vector3 mouseScreenPosition = Input.mousePosition;
+        mouseScreenPosition.Set(mouseScreenPosition.x / Screen.width, mouseScreenPosition.y / Screen.height, 0.0f);
+
+        return mouseScreenPosition;
+    }
+
+    public void OnPostRender()
+    {
+        GL.PushMatrix();
+        GL.LoadOrtho();
+
+        if (isAiming)
+        {
+            LineUtility.DrawLine(originalAimingPosition, originalAimingPosition + originalAimingPosition - currentAimingPosition, ArrowMaterial);
+        }
+
+        GL.PopMatrix();
+    }
+
+    public void OnDrawGizmos()
+    {
+        if (isAiming)
+        {
+            Gizmos.DrawWireSphere(ScreenToWorldPoint(originalAimingPosition), 0.5f);
+            Gizmos.DrawWireSphere(ScreenToWorldPoint(currentAimingPosition), 0.5f);
+
+            Vector3 cubeSize = new Vector3(0.5f, 5.0f, 1.0f);
+            Gizmos.DrawWireCube(ScreenToWorldPoint(GetMouseScreenPosition()) + Vector3.up * cubeSize.y * 0.5f, cubeSize);
+            Vector3 fillSize = cubeSize - Vector3.up * cubeSize.y * (1.0f - GetShootingRatio());
+            Gizmos.DrawCube(ScreenToWorldPoint(GetMouseScreenPosition()) + Vector3.up * fillSize.y * 0.5f, fillSize);
+        }
+
+        
     }
 }
